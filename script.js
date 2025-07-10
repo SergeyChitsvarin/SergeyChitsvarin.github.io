3bd7e5186ee243c1740c5bbf4b08688e1e2e2e53
SergeyChitsvarin <Sergeychits20@gmail.com>
2025-07-09 21:13:55 -0600

    Adding comments

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const logo = document.getElementById("logo");
-  const nav = document.getElementById("nav");
+    // when DOM loaded getting the logo and nav elements by it
+    const logo = document.getElementById("logo");
+    const nav = document.getElementById("nav");
 
-  logo.addEventListener("click", (e) => {
-    if (window.innerWidth <= 768) {
-      e.preventDefault();
-      nav.classList.toggle("active");
-    }
-  });
+    // event listener when logo is clicked
+    logo.addEventListener("click", (e) => {
+        // if using responsive design set as active
+        if (window.innerWidth <= 768) {
+            e.preventDefault();
+            nav.classList.toggle("active");
+        }
+    });
 });
