1380e8a0b8b1b75ea72c8101d97783f7571f8645
SergeyChitsvarin <Sergeychits20@gmail.com>
2025-07-09 21:29:00 -0600

    Dynamically updating logo text to haburger icon when the screen is resized

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const logo = document.getElementById("logo");
     const nav = document.getElementById("nav");
 
+    // If window size is smaller than 768 changing logo text
+    function checkSmallScreen() {
+        if (window.innerWidth <= 768){
+            logo.innerHTML = "☰";
+        }
+        else{
+            logo.innerHTML = "Sergey";
+        }
+    };
+
     // event listener when logo is clicked
     logo.addEventListener("click", (e) => {
         // if using responsive design set as active
@@ -11,4 +21,8 @@ document.addEventListener("DOMContentLoaded", () => {
             nav.classList.toggle("active");
         }
     });
+    checkSmallScreen();
+
+    //Dynamically checking to update logo text when screen is resized
+    window.addEventListener("resize", checkSmallScreen);
 });
